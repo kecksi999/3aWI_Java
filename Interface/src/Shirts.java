edec4b09b5224f8fd072ddfd39e782ba7533a9ba
kecksi999 <jan.keckeis@hotmail.com>
2025-01-31 12:01:03 +0100

    Finished Interfaces

---
+++ Interface/src/Shirts.java
@@ -0,0 +1,19 @@
+public class Shirts implements Product {
+    private String name;
+    private double price;
+
+    public Shirts(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public double getPrice() {
+        return price;
+    }
+}
